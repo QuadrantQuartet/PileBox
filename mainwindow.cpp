5c5e383e948772034cbaef7ec8385f5918648b76
忘忧北萱草 <wybxc@qq.com>
2022-05-09 15:20:04 +0800

    update main & format

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,17 @@
 #include "mainwindow.h"
+
+#include <QMessageBox>
+
 #include "./ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
+    : QMainWindow(parent), ui(new Ui::MainWindow) {
     ui->setupUi(this);
+    connect(ui->pushButton, &QPushButton::clicked, this, []() {
+        QMessageBox msgBox;
+        msgBox.setText("寄！");
+        msgBox.exec();
+    });
 }
 
-MainWindow::~MainWindow()
-{
-    delete ui;
-}
-
+MainWindow::~MainWindow() { delete ui; }
