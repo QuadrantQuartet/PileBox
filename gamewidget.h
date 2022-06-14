2fd90b6e8551c7337541fbd70cfd3d7307f55b45
HerocatUED <1722791503@qq.com>
2022-06-14 15:46:23 +0800

    Box2D 支持

--- gamewidget.h
+++ gamewidget.h
@@ -1,38 +1,29 @@
 #ifndef GAMEWIDGET_H
 #define GAMEWIDGET_H
 
-#include <QWidget>
 #include <QGraphicsScene>
+#include <QWidget>
+#include "box2dWidget/BoxScene.h"
 
 namespace Ui {
 class GameWidget;
 }
 
-class GameWidget : public QWidget
-{
+class GameWidget : public QWidget {
     Q_OBJECT
 
-public:
-    explicit GameWidget(QWidget *parent = nullptr);
+  public:
+    explicit GameWidget(QWidget* parent = nullptr);
     bool eventFilter(QObject* watched, QEvent* event) override;
-    ~GameWidget();
+    void resizeEvent(QResizeEvent* event) override;
+    ~GameWidget() override;
 
-private:
-    Ui::GameWidget *ui;
-    QGraphicsScene* graphicsScene;
-    bool graphicDragging = false;
-    QPoint graphicDragStartPoint;
-    QPoint graphicDragOffset = {0, 0};
+  private:
+    Ui::GameWidget* ui;
+    BoxScene* boxScene;
 
     /// 在绘图区域中添加一个正方形
     void addRect(const QPointF& pos);
-    /// 开始拖拽平移
-    void startDrag(const QPoint& pos);
-    /// 更新拖拽平移过程
-    void updateDrag(const QPoint& pos);
-    /// 结束拖拽平移
-    void endDrag();
-
     /// 缩放绘图区域
     void graphicsScale(double factor);
     /// 平移绘图区域
@@ -42,4 +33,4 @@ private:
     [[nodiscard]] QPointF mapToScene(const QPoint& pos) const;
 };
 
-#endif // GAMEWIDGET_H
+#endif  // GAMEWIDGET_H
